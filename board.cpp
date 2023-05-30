aa9def265ebd028a55c097a654c5382170b7c21f
Mizuki Asai <mimic0924444@gmail.com>
2023-05-30 16:14:41 +0900

    入力時のガイドとして、最初の出力に数字を降る関数printnumberを作成

--- board.cpp
+++ board.cpp
@@ -183,6 +183,19 @@ void cout_generation(Eigen::MatrixXi board,int y, int x){
     }
 
 
+Eigen::MatrixXi printnumber(Eigen::MatrixXi board,int y,int x){
+for(int n = 0; n < x+1; n++){
+    std::cout << n << " ";
+}
+std::cout<< " "<< std::endl;
+for(int i = 0; i < y+1; i++){
+    board(i,0) = i;
+}
+
+return board;
+}
+
+
 int main()
 {
     // create board
@@ -196,7 +209,8 @@ int main()
     onboard = board(y + 2, x + 2);
     std::cout << "0世代"
               << "\n";
-    trimingboard = onboard.block(1, 1, y, x);
+    onboard = printnumber(onboard,y,x);
+    trimingboard = onboard.block(1, 0, y, x+1);
     std::cout << trimingboard << "\n\n";
 
     onboard = first_liveplace(onboard, y, x);
